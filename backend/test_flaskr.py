6d9c98d7546b6cd50ecc8b87ff6e9a10a8e6c784
Charanjeet Arneja <charanjeetsingharneja@gmail.com>
2022-04-25 11:34:19 +0530

    adding more test cases

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -46,7 +46,99 @@ class TriviaTestCase(unittest.TestCase):
         output=json.loads(response.data)
         self.assertEqual(response.status_code,404)
         self.assertEqual(output["success"],False)
-        self.assertEqual(output['message'], 'Bad request')
+        self.assertEqual(output['message'], 'Page not found')
+
+    def test_get_all_categories(self):
+        response = self.client().get('/categories')
+        output = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(output["success"], True)
+        self.assertTrue(output["categories"])
+
+#changes
+
+    def test_question_deletion(self):
+        response = self.client().delete('/questions/4')
+        output = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(output["success"], True)
+
+    def test_question_doesnt_exist_deletion(self):
+        response = self.client().delete('/questions/99999')
+        output = json.loads(response.data)
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(output["success"], False)
+        self.assertEqual(output["message"], "Page not found")
+
+    def test_add_question(self):
+        newQuestion = {
+            'question': 'Test it?',
+            'answer': 'Test',
+            'difficulty': 4,
+            'category': 4
+        }
+        response = self.client().post('/questions', json=newQuestion)
+        output = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(output["success"], True)
+
+    def test_search_questions(self):
+        search = {'searchTerm': '?'}
+        response = self.client().post('/search', json=search)
+        output = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(output['success'], True)
+        self.assertGreater(len(output['questions']), 0)
+
+    def test_search_questions_not_found(self):
+        search = {'searchTerm': 'test_search_questions_not_found'}
+        response = self.client().post('/search', json=search)
+        output = json.loads(response.data)
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(output['success'], False)
+        self.assertEqual(output['message'], 'Page not found')
+
+    def test_questions_in_category(self):
+        response = self.client().get('/categories/4/questions')
+        output = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(output['success'], True)
+        self.assertNotEqual(len(output['questions']), 0)
+        self.assertGreater(output['totalQuestions'], 0)
+        self.assertEqual(output['currentCategory'], 'History')
+
+    def test_questions_in_category_not_found(self):
+        response = self.client().get('/categories/999/questions')
+        output = json.loads(response.data)
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(output['success'], False)
+
+    def test_quiz(self):
+        quiz = {
+            'previous_questions': [13],
+            'quiz_category': {
+                'type': 'Entertainment',
+                'id': '3'
+            }
+        }
+        response = self.client().post('/quizzes', json=quiz)
+        output = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(output['success'], True)
+        self.assertEqual(output['question']['category'], 3)
+
+    def test_quiz_not_found_category(self):
+        quiz = {
+            'previous_questions': [6],
+            'quiz_category': {
+                'type': 'XXX',
+                'id': 'X'
+            }
+        }
+        response = self.client().post('/quizzes', json=quiz)
+        output = json.loads(response.data)
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(output['success'], False)
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
